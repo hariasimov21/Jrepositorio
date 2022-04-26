65376af5f39077b4e412fd677fb2df63333e31b4
hariasimov21 <james18bt@gmail.com>
2022-04-25 23:49:58 -0400

    [Update] archivos contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,39 @@
-import './Contact.scss';
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import "./Contact.scss";
+import { useEffect, useState } from "react";
 
 const Contact = () => {
-    return (
-        <>
-        <div className="Contact">
-            
+  const [letterClass, setletterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setletterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["C", "o", "n", "t", "a", "c", "t", "a", "m", "e"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            architecto repellat eum fuga rem distinctio animi saepe optio. Natus
+            eos quam doloribus rerum nemo minus consequuntur vitae porro modi
+            aliquid?
+          </p>
         </div>
-        </>
-    )
-}
+      </div>
+      <Loader type="cube-transition" />
+    </>
+  );
+};
+
+export default Contact;
